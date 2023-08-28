7399c2bad1031f1d7c212c2ea59494e4ccf883d6
Matej Drlicka <sprattek@gmail.com>
2023-08-28 11:41:28 +0200

    builded

--- lib/models/floor.d.ts
+++ lib/models/floor.d.ts
@@ -25,7 +25,7 @@ export declare class FloorModel extends BaseModel {
     editor?: FloorEditorModel;
     geopoint?: [number, number];
     remoteId?: string;
-    metadata?: Object;
+    metadata?: object;
     constructor(data: any);
     get hasFloorplan(): boolean;
 }
