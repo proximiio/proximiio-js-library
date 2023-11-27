0d89403b74cd1040efe8fa585bc15cf83e9d8312
Matej Drlicka <sprattek@gmail.com>
2023-11-27 11:32:10 +0100

    1.9.44

--- src/components/map/main.ts
+++ src/components/map/main.ts
@@ -956,7 +956,7 @@ export class Map {
       this.map.on('click', 'proximiio-pois-icons', (ev) => {
         this.onShopClick(ev);
       });
-      
+
       this.map.on('click', 'pois-icons', (ev) => {
         this.onShopClick(ev);
       });
