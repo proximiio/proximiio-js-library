3cb845929bee94f68eb1c4066c919de8fced3240
Matej Drlicka <sprattek@gmail.com>
2024-01-26 16:17:12 +0100

    feat(logger): add SelectLogger class to handle logging of selected elements
    - Added a new file `src/components/logger/select.ts` which contains the implementation of the `SelectLogger` class.
    - The `SelectLogger` class has properties for the clicked element's ID, title, and type, as well as optional properties for the kiosk ID and metadata.
    - The constructor of the `SelectLogger` class initializes the properties and calls the `logSelectedElement` method.
    - The `logSelectedElement` method calls the `saveLog` method to send a POST request to the `web-analytics/collector/clicks` endpoint with the `SelectLogger` instance as the payload.
    - If the log saving fails, an error message is logged to the console.
    - Imported the `SelectLogger` class in `src/index.ts` and added it to the exported object.
    
    The `SelectLogger` class is responsible for logging the selected elements and sending the log to the server for further processing and analysis.

---
+++ src/components/logger/select.ts
@@ -0,0 +1,37 @@
+import { axios } from '../../common';
+
+export default class SelectLogger {
+  clickedElementId: string;
+  clickedElementTitle: string;
+  clickedElementType: 'amenity' | 'feature' | 'amenity_category';
+  kioskId?: string;
+  metadata?: Record<string, string>;
+
+  constructor(data: {
+    clickedElementId: string;
+    clickedElementTitle: string;
+    clickedElementType: 'amenity' | 'feature' | 'amenity_category';
+    kioskId?: string;
+    metadata?: Record<string, string>;
+  }) {
+    this.clickedElementId = data.clickedElementId;
+    this.clickedElementTitle = data.clickedElementTitle;
+    this.clickedElementType = data.clickedElementType;
+    this.kioskId = data.kioskId;
+    this.metadata = data.metadata;
+
+    this.logSelectedElement();
+  }
+
+  async saveLog() {
+    try {
+      await axios.post(`web-analytics/collector/clicks`, this);
+    } catch (e) {
+      console.error(`Log saving failed, ${e.message}`);
+    }
+  }
+
+  logSelectedElement() {
+    this.saveLog();
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import Floors from './controllers/floors';
 import Kiosks from './controllers/kiosks';
 import { Map } from './components/map/main';
 import { Select } from './components/select/main';
+import SelectLogger from './components/logger/select';
 
 export default {
   Auth,
@@ -12,4 +13,5 @@ export default {
   Kiosks,
   Map,
   Select,
+  SelectLogger,
 };
