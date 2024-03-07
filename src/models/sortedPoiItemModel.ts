292f91f4d2c27fe0546c1da07ab485c29a8ffe93
Matej Drlicka <sprattek@gmail.com>
2024-03-07 11:29:45 +0100

    1.11.27

--- src/models/sortedPoiItemModel.ts
+++ src/models/sortedPoiItemModel.ts
@@ -1,19 +1,19 @@
-import { Geometry } from "./feature";
-import { FloorModel } from "./floor";
+import { Geometry } from './feature';
+import { FloorModel } from './floor';
 
 export interface SortedPoiItemModel {
-	type: 'Feature';
-	id: string;
-	geometry: Geometry;
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	properties: Record<string, any>;
-	icon: string;
-	category: string;
-	search_query: string;
-	coordinates: number[];
-	isInside: boolean;
-	score: number;
-	foundInDescription: boolean;
-	floor: FloorModel | undefined;
-	floorName: string;
+  type: 'Feature';
+  id: string;
+  geometry: Geometry;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  properties: Record<string, any>;
+  icon: string;
+  category: string;
+  search_query: string;
+  coordinates: number[];
+  isInside: boolean;
+  score: number;
+  foundInDescription: boolean;
+  floor: FloorModel | undefined;
+  floorName: string;
 }
