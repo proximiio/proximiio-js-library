4136aeccc5f6bb116cbd08db62ae1357542a32e9
Matej Drlicka <sprattek@gmail.com>
2024-04-19 11:03:01 +0200

    refactor(sortedPoiItemModel.ts): change icon and category properties to be optional to better reflect their optional nature refactor(sortedPoiItemModel.ts): change isInside property to be optional to better reflect its optional nature

--- src/models/sortedPoiItemModel.ts
+++ src/models/sortedPoiItemModel.ts
@@ -7,11 +7,11 @@ export interface SortedPoiItemModel {
   geometry: Geometry;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   properties: Record<string, any>;
-  icon: string;
-  category: string;
+  icon?: string;
+  category?: string;
   search_query: string;
   coordinates: number[];
-  isInside: boolean;
+  isInside?: boolean;
   score: number;
   foundInDescription: boolean;
   floor: FloorModel | undefined;
