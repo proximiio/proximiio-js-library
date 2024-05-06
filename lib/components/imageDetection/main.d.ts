a47c737114271f9968d594d37ee5c7ae5907767c
Matej Drlicka <sprattek@gmail.com>
2024-05-06 11:58:16 +0200

    published

--- lib/components/imageDetection/main.d.ts
+++ lib/components/imageDetection/main.d.ts
@@ -2,11 +2,16 @@ import { SortedPoiItemModel } from '../../models/sortedPoiItemModel';
 interface Options {
     gVisionApiKey: string;
     pois: SortedPoiItemModel[];
+    cameraText?: string;
     captureButtonText?: string;
     closeButtonText?: string;
+    tryAgainButtonText?: string;
+    confirmButtonText?: string;
     noResultsText?: string;
     resultsHeadingText?: string;
     returnResults?: number;
+    brandColor?: string;
+    dir?: string;
 }
 declare class ImageDetection {
     static init(options: Options, onSelect: (item: SortedPoiItemModel) => void): void;
