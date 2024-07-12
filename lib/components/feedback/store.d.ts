4768c6fa9c3cdf7611ee318e3c46d69a4abb2b85
Matej Drlicka <sprattek@gmail.com>
2024-07-12 08:44:49 +0200

    builded

--- lib/components/feedback/store.d.ts
+++ lib/components/feedback/store.d.ts
@@ -1,9 +1,11 @@
 import Feedback, { IFeedback } from './base';
 export interface IStoreFeedback extends IFeedback {
     storeTitle: string;
+    poi_id: string;
 }
 export default class StoreFeedback extends Feedback {
     storeTitle: string;
+    poi_id: string;
     constructor(data: IStoreFeedback);
     save(): Promise<void>;
 }
